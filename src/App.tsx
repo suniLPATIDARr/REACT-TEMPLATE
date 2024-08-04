0ea837b955ee9ead46a92fe14263b95a1c71dd48
suniLPATIDARr <sunilpatidar1209@gmail.com>
2024-08-04 13:27:48 +0530

    Heading change

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Counter } from './Counter'
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1>React TypeScript Webpack Start Template</h1>
       <Counter />
     </>
   )
